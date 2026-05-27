e9cb20cd6ccfc3db1fc6cc1b3074550028077571
Alex9alexandra <ale02pop@yahoo.com>
2026-05-28 01:23:10 +0300

    i think this time will work

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,6 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 5173,
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, './backend/key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, './backend/cert.pem')),
-    },
   },
   test: {
     environment: 'jsdom',
